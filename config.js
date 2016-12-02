c3d4fec728b2f78bb1338901b7c4e091308dc76e
Vikas Satpute <vikas.satpute@weboniselab.com>
2016-12-02 15:04:35 +0530

    db

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 	'secret': 'ilovescotchyscotch',
-  'mongodb://bank:bank123#@ds015636.mlab.com:15636/heroku_d9lrkfdg'
+  'database': 'mongodb://bank:bank123#@ds015636.mlab.com:15636/heroku_d9lrkfdg'
   //'database': 'mongodb://<bank>:<dbpassword>@ds119508.mlab.com:19508/heroku_d9lrkfdg'
 	//'database': 'mongodb://127.0.0.1/auth'
 
